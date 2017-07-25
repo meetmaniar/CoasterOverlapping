575dfda37a8ab14d108f3a51fb95a5f84fef69d7
Maniar Meet <meetrmaniar@gmail.com>
2017-07-25 17:48:12 -0400

    Length

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 Created on Jul 22, 2017
 
 @author: M_ANIAR
+
 '''
 from scratch import *
-
 
 def f(a):
         a = Decimal(a)
         return a-scratch_sine(a)-(pi/2)
     
-a=bisection(f,1,3)
+
+a=bisection(f,0,10)
 print(a)
-
-
+r=input("Enter Radius:")
+r=Decimal(r)
+l=2*r*(1-(scratch_cosine(a/2)))
+print(l)
