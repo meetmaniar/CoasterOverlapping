43a440c8342c7394f64b30a705612755607a071c
Maniar Meet <meetrmaniar@gmail.com>
2017-07-27 00:38:29 -0400

    xml

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 
 
 
+
+
+
 '''
-Created on Jul 22, 2017
+Created on Jul 10, 2017
 
 @author: M_ANIAR
 
 '''
 from scratch import *
 
+from xml.etree.ElementTree import *
 def f(a):
         a = Decimal(a)
         return a-scratch_sine(a)-(pi/2)
     
 
+
 a=bisection(f,0,10)
 print(a)
 try:
     r=Decimal(input("Enter Radius: "))
+    if(r<0): 
+        raise ValueError
     l=2*r*(1-(scratch_cosine(a/2)))
     print(l)
+    root=Element('Output')
+    tree=ElementTree(root)
+    alphaValue=Element('Alpha')
+    root.append(alphaValue)
+    lengthValue=Element('Length')
+    root.append(lengthValue)
+    alphaValue.text='Value of Alpha is: ' + str(a) + 'Radians'
+    lengthValue.text='Value of length is : ' + str(l)
+    tree.write(open('pojectoutput.xml', 'wb'))
 except ValueError:
     print("Invalid Input")
+
